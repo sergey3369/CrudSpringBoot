15718d58bf07802f0de8ad38780cb2cee09e50a8
sergey3369 <lernbegierig663@gmail.com>
2022-02-08 23:07:15 +0300

    sqsa

--- src/main/java/com/example/demo/dao/DaoImpl.java
+++ src/main/java/com/example/demo/dao/DaoImpl.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public class DaoImpl implements Dao {
 
-    @PersistenceContext
+    @Autowired
     private EntityManagerFactory entityManagerFactory;
 
     @Transactional
