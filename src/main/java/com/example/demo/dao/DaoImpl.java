fa553046723700eb5d207d457225d33eb47d7d7c
sergey3369 <lernbegierig663@gmail.com>
2022-02-08 23:13:25 +0300

    sqsa

--- src/main/java/com/example/demo/dao/DaoImpl.java
+++ src/main/java/com/example/demo/dao/DaoImpl.java
@@ -11,70 +11,40 @@ import java.util.List;
 @Repository
 public class DaoImpl implements Dao {
 
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
+    @PersistenceContext
+    private EntityManager entityManager;
 
-    @Transactional
     @Override
     public List<User> getUsers() {
-        EntityManager entityManager = entityManagerFactory
-                .createEntityManager();
         Query query = entityManager.
                 createQuery("SELECT e FROM User e");
         List<User> list = query.getResultList();
         return list;
     }
 
-    @Transactional
     @Override
     public void saveUser(User user) {
-        EntityManager entityManager = entityManagerFactory
-                .createEntityManager();
-        EntityTransaction entityTransaction = entityManager.getTransaction();
-        entityTransaction.begin();
         entityManager.persist(user);
-        entityManager.close();
-        entityTransaction.commit();
     }
 
-    @Transactional
     @Override
     public User getUser(int id) {
-        EntityManager entityManager = entityManagerFactory
-                .createEntityManager();
-        EntityTransaction entityTransaction = entityManager.getTransaction();
-        entityTransaction.begin();
         Query query = entityManager.
                 createQuery("select u from User u where u.id = :id");
         query.setParameter("id", (long) id);
-        entityTransaction.commit();
         return (User) query.getSingleResult();
     }
 
-    @Transactional
     @Override
     public void deleteUser(Integer id) {
-        EntityManager entityManager = entityManagerFactory
-                .createEntityManager();
-        EntityTransaction entityTransaction = entityManager.getTransaction();
-        entityTransaction.begin();
         Query query = entityManager.
                 createQuery("delete from User u where u.id = :id");
         query.setParameter("id", (long) id);
         query.executeUpdate();
-        entityManager.close();
-        entityTransaction.commit();
     }
 
-    @Transactional
     @Override
     public void update(User user) {
-        EntityManager entityManager = entityManagerFactory
-                .createEntityManager();
-        EntityTransaction entityTransaction = entityManager.getTransaction();
-        entityTransaction.begin();
         entityManager.merge(user);
-        entityManager.close();
-        entityTransaction.commit();
     }
 }
